1674196e15e23b326ba1e647539c46d679411024
pistolove <pistolove@163.com>
2015-07-23 13:55:59 +0800

    House_Robber
    Change-Id: Ibbfd9494553cdbe757548226c3bef012a67c4e9c
    Signed-off-by: pistolove <pistolove@163.com>

---
+++ lettcode/src/leetcode/House_Robber.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+public class House_Robber {
+
+	public static void main(String[] args) {
+		rob(new int[] { 1, 3, 4, 2, 7, 5, 9, 6 });
+	}
+
+	public static int rob(int[] nums) {
+
+		if (nums == null || nums.length == 0)
+			return 0;
+
+		int len = nums.length;
+		int[] rt = new int[len];
+
+		if (len == 1)
+			return nums[0];
+
+		if (len == 2) {
+			return nums[0] > nums[1] ? nums[0] : nums[1];
+		}
+
+		for (int i = 0; i < len; i++) {
+			if (i == 0) {
+				rt[i] = nums[i];
+			} else if (i == 1) {
+				rt[i] = Math.max(rt[i - 1], nums[i]);
+			} else {
+				rt[i] = Math.max(rt[i - 1], rt[i - 2] + nums[i]);
+			}
+		}
+		return rt[len - 1] > rt[len - 2] ? rt[len - 1] : rt[len - 2];
+	}
+}

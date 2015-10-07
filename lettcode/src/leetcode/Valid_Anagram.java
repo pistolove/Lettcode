d4d30c647f662108d819e4157c1502353296c118
pistolove <pistolove@163.com>
2015-10-07 15:00:30 +0800

    valide_anagram 2015-10-07
    Change-Id: Id87594a2f5af30ad111dfd28150eeb1431bba90a
    Signed-off-by: pistolove <pistolove@163.com>

---
+++ lettcode/src/leetcode/Valid_Anagram.java
@@ -0,0 +1,67 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Valid_Anagram {
+
+	public static void main(String[] args) {
+		isAnagram("anagram", "nagaram");
+	}
+
+	public static boolean isAnagram(String s, String t) {
+		if (s == null || t == null)
+			return false;
+
+		if (s.trim().equals(t.trim()))
+			return true;
+
+		if (s.length() != t.length())
+			return false;
+
+		char[] charArray = s.toCharArray();
+		char[] charArray2 = t.toCharArray();
+		Arrays.sort(charArray);
+		Arrays.sort(charArray2);
+
+		return new String(charArray).equals(new String(charArray2));
+	}
+
+	public static boolean isAnagram2(String s, String t) {
+		if (s == null || t == null)
+			return false;
+
+		if (s.trim().equals(t.trim()))
+			return true;
+
+		if (s.length() != t.length())
+			return false;
+
+		char[] charArray = s.toCharArray();
+		char[] charArray2 = t.toCharArray();
+
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+		for (Character c : charArray) {
+			if (!map.containsKey(c)) {
+				map.put(c, 1);
+			} else {
+				map.put(c, map.get(c) + 1);
+			}
+		}
+
+		for (Character c : charArray2) {
+			if (!map.containsKey(c)) {
+				return false;
+			} else {
+				if (map.get(c) <= 0) {
+					return false;
+				} else {
+					map.put(c, map.get(c) - 1);
+				}
+			}
+		}
+
+		return true;
+	}
+}

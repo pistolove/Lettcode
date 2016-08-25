e4a2d8c11de4dcd15ff42b0cf6ef925518023dee
pistolove <pistolove@163.com>
2016-08-25 11:15:17 +0800

    update
    Change-Id: Icfe7a924c1b602631f7807d59f649937f55fa3e4

--- lettcode/src/leetcode/Power_of_Four.java
+++ lettcode/src/leetcode/Power_of_Four.java
@@ -4,26 +4,19 @@ import org.junit.Test;
 
 public class Power_of_Four {
 
+	public static void main(String[] args) {
+		System.err.println(isPowerOfFour(16));
+	}
+	
 	public static boolean isPowerOfFour(int num) {
-		if (num <= 0)
-			return false;
-
-		if (num == 1)
-			return true;
-
-		while (true) {
-			if (num % 4 == 0) {
-				num = num / 4;
-				if (num == 1)
-					return true;
-			} else {
-				return false;
-			}
-		}
+		
+		return num > 0 && ((num & (num - 1))==0) && ((num - 1) % 3 == 0);
 	}
 
 	@Test
 	public void t1() {
 		System.err.println((400 & (400 >> 2)) % 4);
+		
+		System.err.println(10 & 11);
 	}
 }
